784e1eeb21cefe618e743f497209597b426aa1b9
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-04-19 20:31:21 -0300

    Add basic interaction with the TypeRacer game

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 'use strict';
 
 $('document').ready(_ => {
-    const currentQuote = $('.text-to-type').text();
-    console.log(currentQuote);
+    // Get a reference to the interactive html elements.
+    const textElement = $('.text-to-type');
+    const typeArea = $('.type-area');
 
-    $('.type-area').keydown(e => {
-        const userText = currentQuote;
-        console.log(userText);
+    // Instantiate a new typeracer game.
+    const typeRacer = new TypeRacer(textElement.text());
+    console.log(typeRacer);
+
+    typeArea.keypress(_ => {
+        console.log('firing');
+
+        typeRacer.setTypingText(typeArea.val());
+
+        if (typeRacer.match()) {
+            typeArea.val('');
+        }
+
+        // TODO: Display the matches and typed text.
     });
 });
